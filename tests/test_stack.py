d41a77bac74cc135cdc5199b7c4c6e299b42aca5
Ilya Sachkov <sos31337@gmail.com>
2022-06-15 10:02:29 +0300

    Added test stack

---
+++ tests/test_stack.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+def test_stack():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)
+    stack.pop()
+    assert not stack
+
+
+def test_pop_with_empty_stack():
+    stack = []
+    with pytest.raises(IndexError):
+        stack.pop()
